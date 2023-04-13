a103e2b4aa41440d8829cf2ce083c2b8c9bc8584
ocansey abdul wahab <ocanseyabdulwahab2@gmail.com>
2023-04-13 17:10:44 +0000

    Commpleted the MIS welcoming message interface
    Added an input function for student pin
    Added returning messages to the MIS users

--- myapp.py
+++ myapp.py
@@ -2,3 +2,6 @@
 from getpass import getpass 
 student_fullName = input('Enter your full name: ')
 student_id = int(getpass('Enter your student ID: '))
+student_pin = int(input('Enter your pin: '))
+print(f'Welcome to MIS, {student_id}')
+print(f'Mr.{student_fullName}, you have successfully logged into your MIS\n How can we be of help?')
