b43cd61d398fcc58bd1127e8b13e23dc0d515bbd
ocansey abdul wahab <ocanseyabdulwahab2@gmail.com>
2023-04-13 16:56:58 +0000

    Added input functions to code

---
+++ myapp.py
@@ -0,0 +1,4 @@
+# Create a welcoming messsage for a student MIS web 
+from getpass import getpass 
+student_fullName = input('Enter your full name: ')
+student_id = int(getpass('Enter your student ID: '))
